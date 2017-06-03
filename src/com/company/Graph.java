da415fda0bf0ec5e1d1701811281a77892685e1b
wizzard1994 <matik550@gmail.com>
2017-06-04 00:56:08 +0200

    All works fine

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,8 +1,5 @@
 package com.company;
-
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by Mati on 2017-06-01.
@@ -19,43 +16,86 @@ public class Graph {
     }
 
     public void addEdge(int v, int u, int weight){
-        if (u<0 || weight<1)
+        if (u<0 || u>nrOfVertices || v<0 || v>nrOfVertices || weight<1)
             throw new IllegalArgumentException();
         if(!contains(v))
-            add(v,"Not defined");
+            add("Not defined");
         Vertex ver = get(v);
         ver.addEdge(u,weight);
         nrOfEdges++;
     }
 
-    public void add(int v,String name){
-        if (v<0)
+    public void addEdge(String v, String u, int weight){
+        Vertex ver1,ver2;
+        if (weight<1)
             throw new IllegalArgumentException();
-        verticesList.add(new Vertex(v,name));
-        nrOfVertices++;
+        if(!contains(v)) {
+            ver1 = add(v);
+            nrOfVertices++;
+        }
+        else
+            ver1=get(v);
+        if (!contains(u)) {
+            ver2 = add(u);
+            nrOfVertices++;
+        }
+        else
+            ver2=get(u);
+        ver1.addEdge(ver2.getNr(),weight);
+        nrOfEdges++;
     }
 
-    private boolean contains(int v){
-        if (v<0)
-            throw new IllegalArgumentException();
-        boolean contains=false;
-        for (Vertex ver : verticesList)
-            if (ver.getNr()==v)
-                contains=true;
-        return contains;
+    public Vertex add(String name){
+        Vertex vertex=new Vertex(nrOfVertices,name);
+        verticesList.add(vertex);
+        nrOfVertices++;
+        return vertex;
+    }
+
+    public boolean contains(int v){
+        if (validateVertex(v))
+           return true;
+        return false;
+    }
+
+    public boolean contains(String s){
+        if (validateVertex(s))
+            return true;
+        return false;
     }
 
     public Vertex get(int nr){
+        if (validateVertex(nr))
+            return verticesList.get(nr);
+        return null;
+    }
+
+    public Vertex get(String s){
         for (Vertex v : verticesList)
-            if (v.getNr()==nr)
+            if (v.getData().equals(s))
                 return v;
         return null;
+    }
+
+    private boolean validateVertex(int v){
+        if (v<0 || v>=nrOfVertices)
+            return false;
+        return true;
+    }
+
+    private boolean validateVertex(String s){
+        for (Vertex v : verticesList)
+            if (v.getData().equals(s))
+                return true;
+        return false;
     }
 
     public Vertex remove(int nr){
         for (Vertex ver : verticesList) {
             if (ver.getNr() == nr) {
                 verticesList.remove(ver);
+                nrOfVertices--;
+                nrOfEdges-=ver.getEdges();
                 return ver;
             }
         }
@@ -67,14 +107,68 @@ public class Graph {
         if (ver==null)
             return;
         ver.removeEdge(u);
+        nrOfEdges--;
+    }
+
+    public DijkstraAlgorithm shortestPath(int begin, int end){
+        if (!validateVertex(begin) || !validateVertex(end))
+            throw new IllegalArgumentException();
+        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm(this);
+        dijkstraAlgorithm.Dijkstra(verticesList.get(begin));
+        return dijkstraAlgorithm;
+
+    }
+
+    public void printShortestPath(int begin, int end){
+        DijkstraAlgorithm da = shortestPath(begin,end);
+        System.out.print(da.getMinPath(begin, end));
+    }
+
+    public void printShortestPath(String begin, String end){
+        DijkstraAlgorithm da = shortestPath(begin,end);
+        int from=get(begin).getNr();
+        int to=get(end).getNr();
+        System.out.print(da.getMinPath(from, to));
+    }
+
+
+    public DijkstraAlgorithm shortestPath (String begin, String end){
+        if (!validateVertex(begin) || !validateVertex(end))
+            throw new IllegalArgumentException();
+        int from=get(begin).getNr();
+        int to=get(end).getNr();
+        return shortestPath(from,to);
+    }
+
+    public void allReachable(int nr){
+        DijkstraAlgorithm da = shortestPath(nr,nrOfVertices-1-nr);
+        System.out.println("\n\n Wszystkie miasta osiągalne z "+verticesList.get(nr)+":");
+        for (Vertex x : verticesList){
+           if (x.getDijkstraWeight()!=Integer.MAX_VALUE && x.getDijkstraWeight()!=0)
+               System.out.println(x+" "+x.getDijkstraWeight()+" km ");
+        }
+    }
+
+    public void allReachable(String begin){
+        int from = get(begin).getNr();
+        allReachable(from);
     }
 
 
 
-    private class Vertex{
+    @Override
+    public String toString() {
+        for (Vertex v : verticesList) {
+            System.out.print(v+v.getAdjList());
+        }
+        return "\n";
+    }
+
+    private class Vertex implements Comparable<Vertex>{
         private int nr;
         private String data;
         private int edges;
+        private int DijkstraWeight;
         private TreeMap<Integer, Integer> adj;
 
         public Vertex(int nr,String data){
@@ -106,9 +200,51 @@ public class Graph {
             return data;
         }
 
+        public int getDijkstraWeight() {
+            return DijkstraWeight;
+        }
+
+        public void setDijkstraWeight(int dijkstraWeight) {
+            DijkstraWeight = dijkstraWeight;
+        }
+
         @Override
-        public String toString() {
-            return "Nr wierzchołka: "+nr+" Miasto: "+data;
+        public int compareTo(Vertex o) {
+            return Integer.compare(this.DijkstraWeight,o.getDijkstraWeight());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Vertex ver = (Vertex)obj;
+            return getData().equals(ver.getData());
+        }
+
+        @Override
+        public String toString(){
+            StringBuilder stringBuilder=new StringBuilder();
+            stringBuilder.append(" Miasto: ");
+            stringBuilder.append(data);
+            return stringBuilder.toString();
+        }
+
+        public String getVertexNr(){
+            StringBuilder stringBuilder=new StringBuilder();
+            stringBuilder.append("\nNr wierzchołka: ");
+            stringBuilder.append(nr);
+            return stringBuilder.toString();
+        }
+
+        public String getAdjList() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("\n Możemy się dostać bezpośrednio do: \n");
+            adj.forEach((key,value)->{
+                stringBuilder.append(verticesList.get(key));
+                stringBuilder.append(" ");
+                stringBuilder.append(value);
+                stringBuilder.append(" km\n");
+                    });
+            stringBuilder.append("\n");
+            return stringBuilder.toString();
         }
 
         public int getNr() {
@@ -117,6 +253,91 @@ public class Graph {
 
         public String getData() {
             return data;
+        }
+
+        public int getEdges() {
+            return edges;
+        }
+
+        public TreeMap<Integer, Integer> getAdj() {
+            return adj;
+        }
+    }
+
+    private class DijkstraAlgorithm{
+        private Graph graph;
+        private PriorityQueue<Vertex> queue;
+        private int [] pred;
+        private ArrayList<Integer> visited;
+
+        public DijkstraAlgorithm(Graph g){
+            graph=g;
+            queue=new PriorityQueue<>();
+            pred=new int[graph.nrOfVertices];
+            visited=new ArrayList<>();
+        }
+
+        private  void initializeSingleSource(int s){
+            for (Vertex v : verticesList){
+                v.setDijkstraWeight(Integer.MAX_VALUE);
+            }
+            verticesList.get(s).setDijkstraWeight(0);
+        }
+
+        private void relax(int u, int v, int weight){
+            if (verticesList.get(v).getDijkstraWeight()>
+                    verticesList.get(u).getDijkstraWeight()+weight){
+                verticesList.get(v).setDijkstraWeight
+                        (verticesList.get(u).getDijkstraWeight()+weight);
+                pred[v]=u;
+                queue.remove(verticesList.get(v));
+                queue.add(verticesList.get(v));
+            }
+        }
+
+        public void Dijkstra(Vertex v){
+            initializeSingleSource(v.getNr());
+            for (Vertex vex : verticesList)
+            queue.add(vex);
+            while (!queue.isEmpty()){
+                Vertex vertex=queue.poll();
+                for (Map.Entry<Integer,Integer> entry : vertex.getAdj().entrySet()){
+                    Integer other=entry.getKey();
+                    Integer weight=entry.getValue();
+                    relax(vertex.getNr(),other,weight);
+                }
+            }
+
+        }
+
+        public String getMinPath(int begin, int end){
+            StringBuilder s=new StringBuilder();
+            s=shortestPath(begin,end,s);
+            s.append("Jesteś u celu");
+            return s.toString();
+        }
+
+        private StringBuilder shortestPath(int begin, int end, StringBuilder s) {
+            if (end!=begin)
+                shortestPath(begin,pred[end],s);
+            s.append(verticesList.get(end));
+            s.append(" (");
+            s.append(verticesList.get(end).getDijkstraWeight());
+            s.append(") km\n ");
+            s.append("          ||\n           \\/ \n");
+            return s;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder=new StringBuilder();
+            for (Vertex ver : verticesList){
+                stringBuilder.append(ver);
+                stringBuilder.append(" Waga: ");
+                stringBuilder.append(ver.DijkstraWeight);
+                stringBuilder.append("\n");
+            }
+            return stringBuilder.toString();
         }
     }
     
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,52 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+		Graph graph = new Graph();
+		graph.add("Alexandria");
+		graph.add("Blacksburg");
+		graph.add("Charlottesville");
+		graph.add("Danville");
+		graph.add("Fredericksburg");
+		graph.add("Harrisonburg");
+		graph.add("Lynchburg");
+		graph.add("Newport News");
+		graph.add("Richmond");
+		graph.add("Roanoke");
+		graph.add("Virginia Beach");
+
+		graph.addEdge("Alexandria", "Fredericksburg", 50);
+		graph.addEdge("Fredericksburg", "Richmond", 60);
+		graph.addEdge("Richmond", "Charlottesville", 70);
+		graph.addEdge("Richmond", "Lynchburg", 110);
+		graph.addEdge("Richmond", "Danville", 145);
+		graph.addEdge("Richmond", "Newport News", 70);
+		graph.addEdge("Newport News", "Virginia Beach", 35);
+		graph.addEdge("Virginia Beach", "Danville", 210);
+		graph.addEdge("Danville", "Lynchburg", 70);
+		graph.addEdge("Lynchburg", "Charlottesville", 70);
+		graph.addEdge("Lynchburg", "Roanoke", 65);
+		graph.addEdge("Roanoke", "Blacksburg", 40);
+		graph.addEdge("Blacksburg", "Harrisonburg", 140);
+		graph.addEdge("Harrisonburg", "Alexandria", 135);
+		graph.addEdge("Harrisonburg", "Charlottesville", 50);
+
+		graph.addEdge("Fredericksburg","Alexandria", 50);
+		graph.addEdge( "Richmond","Fredericksburg",  60);
+		graph.addEdge( "Charlottesville","Richmond",  70);
+		graph.addEdge( "Lynchburg","Richmond",  110);
+		graph.addEdge( "Danville","Richmond",  145);
+		graph.addEdge( "Newport News","Richmond",  70);
+		graph.addEdge( "Virginia Beach","Newport News",  35);
+		graph.addEdge( "Danville","Virginia Beach",  210);
+		graph.addEdge("Lynchburg","Danville",  70);
+		graph.addEdge( "Charlottesville","Lynchburg",  70);
+		graph.addEdge( "Roanoke","Lynchburg",  65);
+		graph.addEdge( "Blacksburg","Roanoke",  40);
+		graph.addEdge( "Harrisonburg","Blacksburg",  140);
+		graph.addEdge( "Alexandria","Harrisonburg", 135);
+		graph.addEdge( "Charlottesville","Harrisonburg", 50);
+		System.out.println(graph);
+		graph.printShortestPath("Alexandria","Virginia Beach");
+		graph.allReachable("Alexandria");
+	}
 }
